f5a77a97b4f37c5166d15dadb11e703bb9647050
Ana <Ana@nesto.com>
2018-01-22 22:28:10 +0100

    Initial app commit, needs work

---
+++ app/src/main/java/avidovic/zad29_30/MainActivity.java
@@ -0,0 +1,71 @@
+package avidovic.zad29_30;
+
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickAddTitle(View view) {
+
+        //---add a book---
+
+
+         ContentValues values = new ContentValues();
+        values.put("title", ((EditText)
+                findViewById(R.id.txtTitle)).getText().toString());
+        values.put("isbn", ((EditText)
+                findViewById(R.id.txtISBN)).getText().toString());
+        Uri uri = getContentResolver().insert(
+                Uri.parse(
+                        "content://hr.math.provider.contprov/books"),
+                values);
+    }
+
+    public void onClickRetrieveTitles(View view) {
+        //---retrieve the titles---
+        Uri allTitles = Uri.parse(
+                "content://hr.math.provider.contprov/books");
+
+        Cursor c;
+        if (Build.VERSION.SDK_INT <11) {
+            //---before Honeycomb---
+            c = managedQuery(allTitles, null, null, null,
+                    "title desc");
+        } else {
+            //---Honeycomb and later---
+            CursorLoader cursorLoader = new CursorLoader(
+                    this,
+                    allTitles, null, null, null,
+                    "title desc");
+            c = cursorLoader.loadInBackground();
+        }
+
+        if (c.moveToFirst()) {
+            do{
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(
+                                BookProvider._ID)) + ", " +
+                                c.getString(c.getColumnIndex(
+                                        BookProvider.TITLE)) + ", " +
+                                c.getString(c.getColumnIndex(
+                                        BookProvider.ISBN)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
+    }
+
+}
